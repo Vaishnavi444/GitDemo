c68e7da934d92919ff7ca61a58dddcf73c1ff03f
Vaish <vaishnavisathish98@gmail.com>
2023-06-06 13:52:26 +0530

    commit in the afternoon

---
+++ src/test/java/qaclickacademy/demoClass.java
@@ -0,0 +1,13 @@
+package qaclickacademy;
+
+public class demoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello");
+		System.out.println("Hi");
+		System.out.println("Demo");
+	}
+
+}
